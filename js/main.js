bda65bfbd0523551918687404d9fbed40407bcdd
Yuriy_log <ylogachev-sfedu@mail.ru>
2020-12-17 23:19:50 +0300

    SLIDER Adaprive DONE

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
-$(function(){
+$(function () {
 
     $('.slider').slick({
         infinite: false,
         slidesToShow: 3,
         slidesToScroll: 3,
-        arrows:false,
-        dots:true
+        arrows: false,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1174,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+
+        ]
     });
 
-    $('.menu__btn').on('click', function(){
+    $('.menu__btn').on('click', function () {
         $('.menu__list').toggleClass('menu__list--active')
     });
 
